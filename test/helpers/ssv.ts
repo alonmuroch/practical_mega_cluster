e06d6aece90a06ee3d3a621faf901fd2dbc0caa6
Alon Muroch <alonmuroch@gmail.com>
2024-06-09 08:30:24 +0300

    ssv deployment and testing

--- test/helpers/ssv.ts
+++ test/helpers/ssv.ts
@@ -1,11 +1,38 @@
 import hre from 'hardhat';
 import { ethers, upgrades } from 'hardhat';
 import { Address, keccak256, toBytes } from 'viem';
+import { SSVKeys, KeyShares, EncryptShare } from 'ssv-keys';
+
+import validatorKeys from './json/validatorKeys.json';
+import operatorKeys from './json/operatorKeys.json';
+var mockedValidators = validatorKeys as Validator[];
+var mockedOperators = operatorKeys as Operator[];
 
 const nonces = new Map();
+export let owners: any[];
 let lastValidatorId: number = 0;
 let lastOperatorId: number = 0;
 let ssvToken: any;
+
+export type Validator = {
+    id: number;
+    privateKey: string;
+    publicKey: string;
+};
+
+export type Operator = {
+    id: number;
+    operatorKey: string;
+    publicKey: string;
+};
+
+export type Cluster = {
+    validatorCount: number,
+    networkFeeIndex: number,
+    index: number,
+    active: bool,
+    balance: BigInt
+}
 
 export const CONFIG = {
     initialVersion: 'v1.1.0',
@@ -20,7 +47,7 @@ export const CONFIG = {
 };
 
 export const initializeContract = async function () {
-    let owners = await hre.viem.getWalletClients();
+    owners = await hre.viem.getWalletClients();
 
     lastValidatorId = 1;
     lastOperatorId = 0;
@@ -79,5 +106,117 @@ export const initializeContract = async function () {
         ssvNetwork,
         ssvNetworkViews,
         ssvToken,
+    };
+};
+
+export const DataGenerator = {
+    publicKey: (id: number) => {
+        const validators = mockedValidators.filter((item: Validator) => item.id === id);
+        if (validators.length > 0) {
+            return validators[0].publicKey;
+        }
+        return `0x${id.toString(16).padStart(48, '0')}`;
+    },
+    shares: async (ownerId: number, validatorId: number, operatorIds: number[]) => {
+        let shared: any;
+        const validators = mockedValidators.filter((item: Validator) => item.id === validatorId);
+        if (validators.length > 0) {
+            const validator = validators[0];
+            const payload = await getSecretSharedPayload(validator, operatorIds, ownerId);
+            shared = payload.sharesData;
+        } else {
+            shared = `0x${validatorId.toString(16).padStart(48, '0')}`;
+        }
+        return shared;
+    },
+};
+
+const getSecretSharedPayload = async function (validator: Validator, operatorIds: number[], ownerId: number) {
+    const numberIds = operatorIds.map(id => Number(id));
+
+
+    const selOperators = mockedOperators.filter((item: Operator) => item.id !== undefined && numberIds.includes(item.id));
+    const operators = selOperators.map((item: Operator) => ({ id: item.id, operatorKey: item.operatorKey }));
+
+    const ssvKeys = new SSVKeys();
+    const keyShares = new KeyShares();
+
+    const publicKey = validator.publicKey;
+    const privateKey = validator.privateKey;
+
+    const threshold = await ssvKeys.createThreshold(privateKey, operators);
+    const encryptedShares: EncryptShare[] = await ssvKeys.encryptShares(operators, threshold.shares);
+
+    let ownerNonce = 0;
+
+    if (nonces.has(owners[ownerId].address)) {
+        ownerNonce = nonces.get(owners[ownerId].address);
+    }
+    nonces.set(owners[ownerId].address, ownerNonce + 1);
+
+    const payload = await keyShares.buildPayload(
+        {
+            publicKey,
+            operators,
+            encryptedShares,
+        },
+        {
+            ownerAddress: owners[ownerId].address,
+            ownerNonce,
+            privateKey,
+        },
+    );
+    return payload;
+};
+
+export const registerOperators = async function (ssvContract, numOperators: number) {
+    let owners = await hre.viem.getWalletClients();
+
+    for (let i = 0; i < numOperators; i++) {
+        const publicKey = DataGenerator.publicKey(i);
+        const hash = await ssvContract.write.registerOperator([publicKey, CONFIG.minimalOperatorFee], {
+            account: owners[1].account,
+        })
+        await (await hre.viem.getPublicClient()).waitForTransactionReceipt({ hash })
+
+        const events = await ssvContract.getEvents["OperatorAdded"]();
+
+        if (events[0].args["operatorId"] != i+1) {
+            throw "operator not registered"
+        }
+    }
+}
+
+export const bulkRegisterValidators = async function (
+    ssvNetwork,
+    ownerId: number,
+    numberOfValidators: number,
+    operatorIds: number[],
+    minDepositAmount: BigInt,
+    cluster: Cluster,
+) {
+    const validatorIndex = lastValidatorId;
+    const pks = Array.from({ length: numberOfValidators }, (_, index) => DataGenerator.publicKey(index + validatorIndex));
+    const shares = await Promise.all(
+        Array.from({ length: numberOfValidators }, (_, index) =>
+            DataGenerator.shares(ownerId, index + validatorIndex, operatorIds),
+        ),
+    );
+    const depositAmount = minDepositAmount * BigInt(numberOfValidators);
+
+    await ssvToken.write.approve([ssvNetwork.address, depositAmount], {
+        account: owners[ownerId].account,
+    });
+
+    const result = await ssvNetwork.write.bulkRegisterValidator([pks, operatorIds, shares, depositAmount, cluster], {
+        account: owners[ownerId].account,
+    });
+    lastValidatorId += numberOfValidators;
+
+    const events = await ssvNetwork.getEvents["ValidatorAdded"]();
+
+    return {
+        args: events[0].args,
+        pks,
     };
 };
--- test/ssv.ts
+++ test/ssv.ts
@@ -1,6 +1,6 @@
 import hre from 'hardhat';
 import {expect} from "chai";
-import {initializeContract} from "./helpers/ssv";
+import {initializeContract, registerOperators, bulkRegisterValidators, DataGenerator, CONFIG} from "./helpers/ssv";
 
 describe("SSV", function () {
     let ssvNetwork: any;
@@ -20,6 +20,69 @@ describe("SSV", function () {
 
             expect(await ssvViews.read.getNetworkValidatorsCount()).to.equal(0);
             expect(await ssvToken.read.balanceOf([owners[0].account.address])).to.equal('1000000000000000000000');
+        });
+
+        it("register operator", async function () {
+            let owners = await hre.viem.getWalletClients();
+
+            const publicKey = DataGenerator.publicKey(0);
+            const hash = await ssvNetwork.write.registerOperator([publicKey, CONFIG.minimalOperatorFee], {
+                account: owners[1].account,
+            })
+            const reciept = await (await hre.viem.getPublicClient()).waitForTransactionReceipt({ hash })
+            const events = await ssvNetwork.getEvents["OperatorAdded"]();
+
+            expect(events.length).to.equal(1);
+            expect(events[0].args["operatorId"]).to.equal(1);
+            expect(events[0].args["owner"]).to.deep.equal(owners[1].account.address);
+            expect(events[0].args["publicKey"]).to.equal(publicKey);
+            expect(events[0].args["fee"]).to.equal(CONFIG.minimalOperatorFee);
+        });
+
+        it("register operators bulk", async function () {
+            await registerOperators(ssvNetwork,4);
+            let owners = await hre.viem.getWalletClients();
+            expect(await ssvViews.read.getOperatorById([1])).to.deep.equal([
+                owners[1].account.address, // owner
+                CONFIG.minimalOperatorFee, // fee
+                0, // validatorCount
+                '0x0000000000000000000000000000000000000000', // whitelisting contract address
+                false, // isPrivate
+                true,  // active
+            ])
+        });
+
+        it("register validator", async function () {
+            let owners = await hre.viem.getWalletClients();
+            await registerOperators(ssvNetwork,4);
+
+            const result = await bulkRegisterValidators(
+                ssvNetwork,
+                1,
+                4,
+                [1,2,3,4],
+                1000000000000000n,
+                {
+                    validatorCount: 0,
+                    networkFeeIndex: 0,
+                    index: 0,
+                    balance: 0n,
+                    active: true,
+                });
+            expect(result.args.owner).to.deep.equal(owners[1].account.address);
+            expect(result.args.cluster).to.deep.equal({
+                validatorCount: 4,
+                networkFeeIndex: 0n,
+                index: 140n,
+                active: true,
+                balance: 4000000000000000n
+            });
+            expect(result.pks).to.deep.equal([
+                '0xa063fa1434f4ae9bb63488cd79e2f76dea59e0e2d6cdec7236c2bb49ffb37da37cb7966be74eca5a171f659fee7bc501',
+                '0x821b022611c3cdea28669683ec80a930533633fe7b3489d70fdacf68044661ee2bca1d17d3d095c05f639ebe3108784c',
+                '0x88ab00343b787f87de60d1e8a552a69ab5fb3525128c53d68e78a3fe2e157bcce75e96a87e8968460087927552a3c891',
+                '0x9150572051c3496a67207b4caa371dfba34f127318a7aef145ebdba6e0de506c292af31e20831b0c537ab7478508d3e9',
+            ]);
         });
     })
 })
